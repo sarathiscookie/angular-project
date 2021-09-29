2f46edb893ac0e6d730a571afaf645d5aa6326b9
Sarath T S <sarathiscookie@hotmail.com>
2021-09-29 20:41:11 +0530

    Component Interaction: Binding (@Input)

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ChildComponent implements OnInit {
 
   message = 'Hello from child.';
+
+  @Input() passValueFromParentTOChild: string = '';
 
   constructor() { }
 
