315447da22c296a0d91353ddb1a389fb94b9950d
Sarath T S <sarathiscookie@hotmail.com>
2021-10-01 23:47:44 +0530

    ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
