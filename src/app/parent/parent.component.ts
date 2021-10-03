821254fda5de9bd730dc2518dda96e85976c4c4f
Sarath T S <sarathiscookie@hotmail.com>
2021-10-03 23:56:42 +0530

    Property binding

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+  isDisabled = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 }
