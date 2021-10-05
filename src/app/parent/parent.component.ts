b41458d52c1e6bb9c7344d856f2fb0302575b85c
Sarath T S <sarathiscookie@hotmail.com>
2021-10-05 21:37:38 +0530

    Clean up

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
-
-  isDisabled = true;
-
+  
   constructor() { }
 
   ngOnInit(): void {
