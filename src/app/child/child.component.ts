9e28e42ffa23cc218743dc9fa1194b9adbf06eb1
Sarath T S <sarathiscookie@hotmail.com>
2021-10-06 14:15:20 +0530

    Event binding

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Output() emitEvent = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  emitEventFunction() {
+    this.emitEvent.emit('Event emitted from child');
   }
 
 }
