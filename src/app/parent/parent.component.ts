140663334c980ace4056bddba257eecba7a97f6d
Sarath T S <sarathiscookie@hotmail.com>
2021-10-13 15:33:02 +0530

    Clean up

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -8,54 +8,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class ParentComponent implements OnInit {
 
-  isSpecial = false;
-
-  isActive = false;
-
-  classes = {};
-
-  tdClass = {};
-
-  trClass = {};
-
-  styles = {};
-
   constructor() { }
 
   ngOnInit(): void {
-    this.setClasses();
-    this.setStyles();
-    this.setTrClass();
-    this.setTdClass();
   }
-
-  setClasses() {
-    this.classes = {
-      'table': true,
-      'table-stripped': true,
-      'table-secondary': false,
-      'table-hover': true 
-    }
-  }
-
-  setTdClass() {
-    this.tdClass = {
-      'table-success': true
-    }
-  }
-
-  setTrClass() {
-    this.trClass = {
-      'table-warning': true
-    }
-  }
-
-  setStyles() {
-    this.styles = {
-      'font-size': 'small',
-      'font-style': 'italic',
-      'font-weight': 'bold'
-    }
-  }
-
 }
