8a2312672499bd51b4b2a76fca158541c6d52488
Sarath T S <sarathiscookie@hotmail.com>
2021-10-13 14:06:08 +0530

    Directives: Built-in attribute directives - ngClass, ngStyle, ngModel

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -5,11 +5,57 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
+
 export class ParentComponent implements OnInit {
+
+  isSpecial = false;
+
+  isActive = false;
+
+  classes = {};
+
+  tdClass = {};
+
+  trClass = {};
+
+  styles = {};
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setClasses();
+    this.setStyles();
+    this.setTrClass();
+    this.setTdClass();
+  }
+
+  setClasses() {
+    this.classes = {
+      'table': true,
+      'table-stripped': true,
+      'table-secondary': false,
+      'table-hover': true 
+    }
+  }
+
+  setTdClass() {
+    this.tdClass = {
+      'table-success': true
+    }
+  }
+
+  setTrClass() {
+    this.trClass = {
+      'table-warning': true
+    }
+  }
+
+  setStyles() {
+    this.styles = {
+      'font-size': 'small',
+      'font-style': 'italic',
+      'font-weight': 'bold'
+    }
   }
 
 }
