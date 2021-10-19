debe8fee58a20a31bd3648eb9fd6ca6f04709783
Sarath T S <sarathiscookie@hotmail.com>
2021-10-19 09:40:31 +0530

    Custom attribute directives dynamic values

--- src/app/highlighter.directive.ts
+++ src/app/highlighter.directive.ts
@@ -1,12 +1,26 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
 export class HighlighterDirective {
 
+  @Input() appHighlighter = '';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.appHighlighter);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
   constructor(private el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'magenta';
+    el.nativeElement.style.backgroundColor = this.appHighlighter;
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 
 }
