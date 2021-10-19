5e66eaa577b8a06ff51ed2f7d43d3f3f0f3b323e
Sarath T S <sarathiscookie@hotmail.com>
2021-10-19 09:43:46 +0530

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { CardsComponent } from './cards/cards.component';
-import { HighlighterDirective } from './highlighter.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
     ChildComponent,
-    CardsComponent,
-    HighlighterDirective
+    CardsComponent
   ],
   imports: [
     BrowserModule
--- src/app/highlighter.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlighterDirective } from './highlighter.directive';
-
-describe('HighlighterDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlighterDirective();
-    expect(directive).toBeTruthy();
-  });
-});
