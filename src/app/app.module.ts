8d485694f0075dcae51460e6f744a7327b03c236
Sarath T S <sarathiscookie@hotmail.com>
2021-10-24 22:34:16 +0530

    Binding (data, property, class, event, twoway): Interpolation "{{}}", Banana "()", Subcript "[]"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
@@ -14,7 +15,8 @@ import { CardsComponent } from './cards/cards.component';
     CardsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
